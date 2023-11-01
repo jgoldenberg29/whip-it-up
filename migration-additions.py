ea36024f38830d62d82ba3d31b5af5f16ef0255b
Josh Goldenberg <129535504+jgoldenberg29@users.noreply.github.com>
2023-10-31 20:06:54 -0400

    set up for docker deploy

---
+++ migration-additions.py
@@ -0,0 +1,8 @@
+# import os
+# environment = os.getenv("FLASK_ENV")
+# SCHEMA = os.environ.get("SCHEMA")
+
+
+# between upgrade and downgrade
+# if environment == "production":
+#         op.execute(f"ALTER TABLE users SET SCHEMA {SCHEMA};")
